e0ff1012668724d49a37bacc173948d0b09f6781
Lucas Oliveira <lukorei@gmail.com>
2019-12-10 02:16:33 -0300

    separing in modules

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 fs = require('fs')
 
+const replaceTemplate = require('./modules/replaceTemplate')
 
 
 //Sync
@@ -27,21 +28,6 @@ fs = require('fs')
 // console.log('Will read file')
 
 
-const replaceTemplate = (temp, product)=>{
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
-    output = output.replace(/{%IMAGE%}/g, product.image)
-    output = output.replace(/{%PRICE%}/g, product.price)
-    output = output.replace(/{%FROM%}/g, product.from)
-    output = output.replace(/{%PRODUCTNURIENTS%}/g, product.nutrients)
-    output = output.replace(/{%QUANTITY%}/g, product.quantity)
-    output = output.replace(/{%DESCRIPTION%}/g, product.description)
-    output = output.replace(/{%ID%}/g, product.id)
-
-    if(!product.organic){
-        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
-    }
-    return output   
-}
 
 
 const http = require('http')
